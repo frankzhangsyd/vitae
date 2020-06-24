9a18e94d76d08b572368ca3f1a330c97acd62b21
Frank <frank.dingrui@gmail.com>
2020-06-24 22:43:42 +1000

    Upload

---
+++ update_resume.R
@@ -0,0 +1,28 @@
+rmarkdown::render(input = "resume_html.Rmd",
+                  output_file = "output/vitae.html",
+                  output_options = list(
+                    css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
+                    self_contained = TRUE
+                  ),
+                  params = list(
+                    doctype = "HTML"
+                  )
+            )
+
+rmarkdown::render(input = "resume_pdf.Rmd",
+                  output_file = "pdf.html",
+                  output_options = list(
+                    css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
+                    self_contained = TRUE
+                  ),
+                  params = list(
+                    doctype = "HTML"
+                  )
+            )
+
+
+pagedown::chrome_print(input = "pdf.html",
+                       output = "output/Frank-Zhang-CV.pdf")
+
+fs::file_copy("vitae.html","E:/work project/ding-rui.org/content/vitae.html",overwrite = TRUE)
+
